61fd9fe4b6e1ee9a51e9beb40e2ca3a0bdb151d2
Naredra1501 <narendraramesh15@gmail.com>
2026-04-05 14:42:33 +0530

    Update: Config changed

--- config/db.js
+++ config/db.js
@@ -2,7 +2,13 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
     try {
-        const conn = await mongoose.connect(process.env.MONGO_URI);
+        const conn = await mongoose.connect(process.env.MONGO_URI, {
+            serverApi: {
+                version: '1',
+                strict: true,
+                deprecationErrors: true,
+            }
+        });
         console.log(`MongoDB Connected: ${conn.connection.host}`);
     } catch (err) {
         console.error(`Error: ${err.message}`);

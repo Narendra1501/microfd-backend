161bd3ceb6b9bb1ca0c407834b8f13ad54df0fe8
Naredra1501 <narendraramesh15@gmail.com>
2026-04-05 15:16:08 +0530

    Update: Added credentials

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
     try {
-        const conn = await mongoose.connect(process.env.MONGO_URI, {
+        const conn = await mongoose.connect(process.env.MONGO_URI || 'mongodb+srv://narendraramesh15_db_user:@narendra15R@1@cluster0.zieajvb.mongodb.net/?appName=Cluster0', {
             serverApi: {
                 version: '1',
                 strict: true,

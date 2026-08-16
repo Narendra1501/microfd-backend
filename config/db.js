ca16ea1854c046498f2724c559c8830ee8f33115
Naredra1501 <narendraramesh15@gmail.com>
2026-08-16 23:27:36 +0530

    Update: credentials changed

--- config/db.js
+++ config/db.js
@@ -2,8 +2,8 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
     try {
-        const conn = await mongoose.connect('mongodb+srv://naren_ramesh:2pTCb8bOW4nKqAms@cluster0.cfoeua7.mongodb.net/?appName=Cluster0', {
-            // const conn = await mongoose.connect('mongodb://127.0.0.1:27017/microfeedback', {
+        const conn = await mongoose.connect('mongodb+srv://naren_ramesh:KNk22UAergyi7wJ8@cluster0.cfoeua7.mongodb.net/?appName=Cluster0', {
+        // const conn = await mongoose.connect('mongodb://127.0.0.1:27017/microfeedback', {
             serverApi: {
                 version: '1',
                 strict: true,
